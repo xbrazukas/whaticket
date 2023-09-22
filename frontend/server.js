828155b13bcc2664ffe9f69cd565be0cce597ad1
root <root@vmi1299587.contaboserver.net>
2023-09-22 11:26:37 -0300

    alterações personalização

--- frontend/server.js
+++ frontend/server.js
@@ -2,10 +2,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-require('dotenv').config();
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(process.env.SERVER_PORT);
+app.listen(3000);
 
